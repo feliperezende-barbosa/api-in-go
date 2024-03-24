cfbc36d7140f58acea075ff4479dd3ffe57455f7
Felipe <feliperezende.barbosa@gmail.com>
2024-03-23 21:51:15 -0300

    chore: creating a mock hhtp server

--- api/api_test.go
+++ api/api_test.go
@@ -10,42 +10,43 @@ import (
 
 	"github.com/feliperezende-barbosa/api-in-go/internal/domain"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 )
 
-type TestAlbumApi struct {
-	testAlbumRepository domain.AlbumRepository
-}
-
 var (
-	newRecord = httptest.NewRecorder()
-	router    = setUpRouter()
+	router = setUpRouter()
 )
 
-var albums = []domain.Album{
-	{ID: 1, Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: 2, Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: 3, Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+var albums = []*domain.Album{
+	{ID: uuid.New(), Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: uuid.New(), Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: uuid.New(), Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
 func setUpRouter() *gin.Engine {
 	r := gin.Default()
+	r.GET("/albums", func(c *gin.Context) {
+		c.String(200, "opa")
+	})
 	return r
 }
 
 // TestGetAlbums calls getAlbums, checking for a valid return value
 func TestGetAlbums(t *testing.T) {
-	jsonValue, _ := json.Marshal(albums)
-	req, _ := http.NewRequest("GET", "/albums", bytes.NewBuffer(jsonValue))
+	router.Run("localhost:8080")
+	newRecord := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/albums", nil)
 	router.ServeHTTP(newRecord, req)
 
 	assert.Equal(t, http.StatusOK, newRecord.Code)
+	// assert.Equal(t, "opa", newRecord.Body.String())
 }
 
 // TestGetAlbumById calls getAlbumById with an id, checking for a valid return value
 func TestGetAlbumById(t *testing.T) {
-	albumId := domain.Album{ID: 1}
-
+	albumId := domain.Album{ID: uuid.New()}
+	newRecord := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", fmt.Sprintf("/albums/%v", albumId.ID), nil)
 	router.ServeHTTP(newRecord, req)
 
@@ -54,8 +55,8 @@ func TestGetAlbumById(t *testing.T) {
 
 // TestGetAlbumById calls getAlbumById with an id, checking for an error.
 func TestErrorGetAlbumById(t *testing.T) {
-	albumId := domain.Album{ID: 0}
-
+	albumId := domain.Album{ID: uuid.New()}
+	newRecord := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", fmt.Sprintf("/albums/%v", albumId.ID), nil)
 	router.ServeHTTP(newRecord, req)
 
@@ -65,8 +66,8 @@ func TestErrorGetAlbumById(t *testing.T) {
 
 // TestPostAlbum calls postAlbum with an album, checking for a valid return value
 func TestPostAlbum(t *testing.T) {
-	newAlbum := domain.Album{ID: 4, Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99}
-
+	newAlbum := domain.Album{ID: uuid.New(), Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99}
+	newRecord := httptest.NewRecorder()
 	jsonValue, _ := json.Marshal(newAlbum)
 	req, _ := http.NewRequest("POST", "/albums", bytes.NewBuffer(jsonValue))
 	router.ServeHTTP(newRecord, req)
@@ -76,8 +77,8 @@ func TestPostAlbum(t *testing.T) {
 
 // TestUpdateAlbumById calls updateAlbumById with an album, checking for a valid return value
 func TestUpdateAlbumById(t *testing.T) {
-	newAlbum := domain.Album{ID: 4, Title: "Sarah Vaughan and Me", Artist: "Sarah Vaughan", Price: 39.99}
-
+	newAlbum := domain.Album{ID: uuid.New(), Title: "Sarah Vaughan and Me", Artist: "Sarah Vaughan", Price: 39.99}
+	newRecord := httptest.NewRecorder()
 	jsonValue, _ := json.Marshal(newAlbum)
 	req, _ := http.NewRequest("PUT", fmt.Sprintf("/albums/%v", newAlbum.ID), bytes.NewBuffer(jsonValue))
 	router.ServeHTTP(newRecord, req)
@@ -87,35 +88,10 @@ func TestUpdateAlbumById(t *testing.T) {
 
 // TestDeleteAlbumById calls deleteAlbumById with an id, checking for a valid return value
 func TestDeleteAlbumById(t *testing.T) {
-	albumId := domain.Album{ID: 3}
-
+	albumId := domain.Album{ID: uuid.New()}
+	newRecord := httptest.NewRecorder()
 	req, _ := http.NewRequest("DELETE", fmt.Sprintf("/albums/%v", albumId.ID), nil)
 	router.ServeHTTP(newRecord, req)
 
 	assert.Equal(t, http.StatusOK, newRecord.Code)
 }
-
-// func TestAlbumApi_GetAlbums(t *testing.T) {
-// 	type fields struct {
-// 		albumRepository domain.AlbumRepository
-// 	}
-// 	type args struct {
-// 		c *gin.Context
-// 	}
-// 	tests := []struct {
-// 		name   string
-// 		fields fields
-// 		args   args
-// 	}{
-// 		// TODO: Add test cases.
-// 		fields.
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			a := &fields{
-// 				albumRepository: tt.fields.albumRepository,
-// 			}
-// 			a.albumRepository.GetAlbums(tt.args.c)
-// 		})
-// 	}
-// }
