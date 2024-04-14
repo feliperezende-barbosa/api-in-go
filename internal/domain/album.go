4cf64746c060db48254926efc2315587e8dd2c6e
Felipe <feliperezende.barbosa@gmail.com>
2024-04-13 22:52:17 -0300

    chore: goiing back the uuid for ID in Album

--- internal/domain/album.go
+++ internal/domain/album.go
@@ -1,10 +1,12 @@
 package domain
 
+import "github.com/google/uuid"
+
 type Album struct {
-	ID     int     `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+	ID     uuid.UUID `json:"id"`
+	Title  string    `json:"title"`
+	Artist string    `json:"artist"`
+	Price  float64   `json:"price"`
 }
 
 type AlbumRepository interface {
